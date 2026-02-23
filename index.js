4d503371afba7132722f130803a7a9950cfd5ca0
Kuldeep Patel <kuldeep.patel.cg@gmail.com>
2026-02-23 20:23:05 +0530

    resolve some erroe

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
-const cors = require('cors');
+// const cors = require('cors');
 const app = express();
-app.use(cors())
+// app.use(cors())
 
 const users = [
   { id: 1, name: "Arjun", role: "student" },
