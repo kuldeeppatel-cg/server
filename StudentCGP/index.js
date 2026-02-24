303552a042767e782a7fbf8246343c7355a834f6
Kuldeep Patel <kuldeep.patel.cg@gmail.com>
2026-02-24 10:15:28 +0530

    changes in cros property

--- StudentCGP/index.js
+++ StudentCGP/index.js
@@ -1,8 +1,8 @@
 const express = require("express");
-const cors = require("cors");
+const cors = require("cros");
 
 const app = express();
-app.use(cors());
+app.use(cros());
 
 const students = [
   {
